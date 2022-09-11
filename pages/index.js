068aad741554e0cd026f56aecdc2cc31e17bcc46
Zikri Azzuri <zikriazzuri5@gmail.com>
2022-09-11 11:55:31 +0700

    add button event

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import {useColorMode, Heading, Text, Flex, Stack} from '@chakra-ui/react';
+import {useColorMode, Heading, Text, Flex, Stack, Button} from '@chakra-ui/react';
 
 import Container from "../components/Container";
 
@@ -31,6 +31,7 @@ export default function Index() {
       >
         <Heading mb={2}>Hi, I am Zikri Azzuri</Heading>
         <Text color={colorSecondary[colorMode]}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</Text>
+        <Button data-splitbee-event="Button Click" data-splitbee-event-type="Resume">View Resume</Button>
       </Flex>
     </Stack>
     </Container>
